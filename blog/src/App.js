c7b35719c121d18e679d8c56e5b5ffbce0f7c7fb
DESKTOP-NJUD9BC\jiin <yunhj@gmail.com>
2024-01-25 16:47:15 +0900

    8강 map 으로 반복문 돌리기

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,9 +4,17 @@ import React, { useState } from 'react';
 function App() {
   let post = '강남우동맛집';
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [따봉, 따따봉] = useState(0);
+  let [따봉, 따따봉] = useState([0, 1, 2]);
   let [modal, setModal] = useState(false);
   // 현재 ui 의 상태를 담아내는 자료형임.
+  // [1, 2, 3].map(function () { console.log(1) });
+  /* map 함수 안에는 콜백 함수를 만든다.map의 괄호에 넣는것. 
+  array 의 개수만큼 반복한다.
+  파라미터를 하나 새롭게 만들
+  return 을 새롭게 만들어 return 하면 array 의 자료를 담아줌 . 그리고 array 의 개수만큼 반복함. 
+   */
+
+
   return (
     <div className="App">
       <div className="App-black-nav">
@@ -40,13 +48,22 @@ function App() {
         글제목변경(copy)
       }}>수정3</button>
       {/* ...을 넣으면 화살표를 새롭게 바꿔달란 뜻으로 새로운 state로 인식하게 됨. 
-    기존의 state가 array 혹은 object 타입이라면  ...을 통해 새로운copy deep copy 가 되기 때문에 가능해짐.*/}
+    기존의 state가 array 혹은 object 타입이라면  ...을 통해 새로운copy deep copy 가 되기 때문에 가능해짐.
+    두 개의 파라미터를 반복하고 싶을 때는 반복문이 돌때마다 1씩 증가하는 함수. 
+     {
+
+      글제목.map(function()){return (<div className = "list"> </div>)}
+     }
+    
+    */}
+
+
       <button onClick={() => {
         let copy = [...글제목];
         copy.sort();
         글제목변경(copy);
       }}> 가나다순 정렬</button>
-      <div className="list">
+      {/* <div className="list">
         <h4> {글제목[0]}<span onClick={() => { 따따봉(따봉 + 1) }}>👍</span>{따봉}</h4>
         <p>2월 17일 발행임</p>
       </div>
@@ -58,43 +75,42 @@ function App() {
         <h4 onClick={()=>{ setModal(!modal)
         }}> {글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
-      { modal == true ? <Modal /> : null} 
-      {/* 컴포넌트 modal 역할을 하는 컴포넌트 역할을함 
-          1. function 작명(){}
-          function 은 다른 함수 밖에 만들어야 함.
-          작명은 대문자 
-          2. funtion  안에 return() 만들기
-          return 안에는 하나의 태그로 시작해서 하나의 태그로 끝나야함 두개 div 병렬은 Nooooo
-         참고 1. div 를 여러개 넣고 싶으면 큰 div안에 넣으면 됨. 이때 의미없는 div가 되기때문에 <> </> 로 해도 됨.
-         컴포넌트 언제쓰면 좋을까?
-         - 반복적으로 사용하는 html 을 축약할때 
-         - 큰 페이지들을 하나의 컴포넌트로
-         - 자주 변경되는 html ui 등을 컴포넌트로 만들면 성능상 좋을 수도 있음.
-         
-         컴포넌트 단점.
-         - state 가져다 쓸때 문제 생김
-        
+      </div> */}
+      {
+        글제목.map(function (a, i) {
 
-        */}
-      
-      {/* 동적인 UI만들기 3단계
-          1. HTML , CSS 사용하여 미리 디자인 완성
-          2. UI 의 현재 상태를 STATE로 저장 -> 어떻게 하나?
-          3. STATE 에 따라 UI가 어떻게 보일지 작성
-        2. 는 let [modal, setModal] = useState(); 방식으로 미리 지정하면 됨. 
-        모달창의 경우는 보이고 안보이고의 상태기 때문에 ()안에 조건문 false로 줌.
-        let [modal, setModal] = useState(false);
-        state 가 fasle 면 안보이게 true 면 보이게 
-        if(){} 특정경우에만 코드 실행하고 싶으면 if만 if를 쓸 수 없음 때문에 삼항연산자로 사용함.
+          return (<div className='list' key={i}>
+            {/* 생각생각생각생각
+            아 만약에 자바스크립트였으면 
+            i 클릭하면 i 번째 배열 +1 해주면 되는거아냐?
+            */}
+            <h4>{글제목[i]}<span onClick={() => {
+              let copy = [...따봉]
+              copy[i] = copy[i] + 1;
+              따따봉(copy);
+            }}>👍</span>{따봉[i]}</h4>
+            <p> 2월 17일 발행</p>
+          </div>
+          )
+        })
+      }
+      {modal == true ? <Modal /> : null}
+      {
+        [1, 2, 3].map(function () {
+          return <div>'안녕' </div>
+        })
+        /* 중괄호 안에서는 map 을 통해 반복문을 쓴다.  
+        why?  map 을 쓰고나면 앞에 있는 개수 길이만큼 빈배열이 생긴다.  
 
-        조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드 1== 1 ? '맞음' : '틀림'  과 같은 식으로 
-        state가 TRUE 면 보여주고 NULL 은 아무것도 안남김. 
-        스위치와 기걔를 만들어 준당므 스위치를 건드리면  떧
-        3번째 제목을 누르면 모달창이 뜸.  
+        */
+        /* 실제 글 데이터 개수만큼 반복 하고 싶으면 어떻게 할까? 
+        파라미터 a를 넣어주면 됨 a는 반복문이 돌때마다 만들어지는 하나하나의 데이터!
+        파라미터 2개를 넣으면 반복문이 돌때마다 1씩 증가하는 정수 .
+        map 은 왼쪽 array 자료 만큼 내부코드 실행해줌. return 오른쪽에 있는걸 array 로 담아줌.
+        유용한 파라미터 2 개 됨.  
+        */
 
-        */}
-        {/*  */}
+      }
     </div>
   );
 }
